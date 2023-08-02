3076792e121e4d5fcdc5c3c76ddd9e0dc93828e1
Virgilio Macero <virgiliomacero@gmail.com>
2023-08-02 15:38:21 -0400

    All CRUD working

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+    ];
 
     public function user(){
 
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -2,15 +2,42 @@
 
 <label class="uppercase text-white text-xs">Titulo</label>
 
-<input type="text" name="title" class="rounded border-gray-200 w-full mb-4" value={{$post->title}}>
+<span class="text-red-600 text-xs">
+    @error('title') 
+
+    {{$message}}
+
+    @enderror
+</span>
+
+<input type="text" name="title" class="rounded border-gray-200 w-full text-black mb-4" value={{ old('title',$post->title) }}>
+
+<label class="uppercase text-white text-xs">Slug</label>
+
+<span class="text-red-600 text-xs">
+    @error('slug') 
+
+    {{$message}}
+
+    @enderror
+</span>
+
+<input type="text" name="slug" class="rounded border-gray-200 w-full text-black mb-4" value={{ old('slug',$post->slug) }}>
 
 <label class="uppercase text-white text-xs">Contenido</label>
-<textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4">{{$post->body}}</textarea>
+<label class="text-red-600 text-xs">
+    @error('body')
+    
+    {{$message}}
 
-<div>
+    @enderror
+</label>
+<textarea name="body" rows="5" class="rounded text-black border-gray-200 w-full mb-4">{{ old('body',$post->body) }}</textarea>
 
-    <a href="{{route('posts.index')}}" class="text-indigo-600">Volver</a>
+<div class="flex items-center justify-between">
 
-    <input type="submit" value="Enviar" class="bg-white text-black rounded px-4 py-2 hover:cursor-pointer">
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
+
+    <input type="submit" value="Guardar" class="bg-white text-black rounded px-4 py-2 hover:cursor-pointer">
 
 </div>
